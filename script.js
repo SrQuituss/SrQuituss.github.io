14c5ab3b6b923d7f9877afe7d60a8b35cd967040
SrQuituss <102283326+SrQuituss@users.noreply.github.com>
2023-06-02 15:00:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,57 @@ document.getElementById('btn-inicio').addEventListener('click', function() {
 });
 
 function escribirLetraPorLetra(texto, elemento) {
+    
     let i = 0;
-  
+    let barra = false;
+    let parpadeoTimeoutId;
+
     function escribir() {
         if (i < texto.length) {
             if (elemento.textContent.length>1){ 
-                elemento.textContent = elemento.textContent.slice(0, -1);
+                quitarBarra();
             }
-            elemento.textContent += texto.charAt(i) + '|';
+            elemento.textContent += texto.charAt(i);
+            añadirBarra();
             i++;
             setTimeout(escribir, 100); 
         } 
         else {
             setTimeout(reiniciar, 5000); 
+            parpadeoTimeoutId = setTimeout(parpadeoBarra, 1000);
         }
+    }
+
+    function añadirBarra()
+    {
+        elemento.textContent += '|';
+        barra = true;
+    }
+
+    function quitarBarra()
+    {
+        elemento.textContent = elemento.textContent.slice(0, -1);
+        barra = false;
+    }
+
+    function parpadeoBarra()
+    {
+        if (barra) {
+            quitarBarra();
+        }
+        else {
+            añadirBarra();
+        }
+        parpadeoTimeoutId = setTimeout(parpadeoBarra, 500);
     }
   
     function reiniciar() {
         elemento.textContent = ""; 
         i = 0; 
+        clearTimeout(parpadeoTimeoutId);
         escribir(); 
     }
-  
+
     escribir();
 }
 
