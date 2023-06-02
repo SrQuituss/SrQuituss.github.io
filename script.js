5b94143a1b45427fe6581cbdc212c378779c37c7
SrQuituss <102283326+SrQuituss@users.noreply.github.com>
2023-06-02 13:43:13 +0200

    letterByLetter

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById('btn-inicio').addEventListener('click', function() {
     window.location.href = 'index.html';
 });
+
+function escribirLetraPorLetra(texto, elemento) {
+    let i = 0;
+    const intervalo = setInterval(function() {
+        if (i < texto.length) {
+            elemento.textContent += texto.charAt(i);
+            i++;
+        }
+        else {
+            clearInterval(intervalo);
+        }
+    }, 100);
+} 
+
+const textoCompleto = "Bienvenido/a a mi portfolio";
+const elementoTexto = document.getElementById("txt-inicio");
+escribirLetraPorLetra(textoCompleto, elementoTexto);
