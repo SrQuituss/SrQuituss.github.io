f8a6a99ba39e8b55344da74f6362c7cab05dd8c4
SrQuituss <102283326+SrQuituss@users.noreply.github.com>
2023-06-16 15:32:22 +0200

    update

--- Proyectos/proyectos.js
+++ Proyectos/proyectos.js
@@ -1,5 +1,5 @@
 const botonInicio = document.getElementById('btn-inicio');
 
 botonInicio.addEventListener('click', function() {
-    window.location.href = 'index.html';
+    window.location.href = '../index.html';
 });
